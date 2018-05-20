688b94f0e9f22a8b4f8bb8a55de93a2fce66e67c
Donald Blanc <dxb3662@rit.edu>
2018-05-20 12:15:09 -0400

    defining the routes

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 import express from 'express';
+import data from './app/data/friends';
+import bodyparser from 'body-parser';
+import path from 'path';
+import  apiRoutes from './app/routing/apiRoutes';
+import htmlRoutes from './app/routing/htmlRoutes';
+
+
+//require("./app/routing/apiRoutes")(app); 
+
+
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 
-app.listen(PORT, () =>
- console.log(`erver listening on: http://localhost:${PORT} `)
 
-);
+apiRoutes(app);
+htmlRoutes(app);
+// app.get('/', (req,res) =>{
+//     res.send(` A route on the / route on port ${PORT} `);
+
+// });
+
+app.listen(PORT, () =>{ 
+ console.log(`Server listening on: http://localhost:${PORT} `);
+ console.log(data);
+
+});
